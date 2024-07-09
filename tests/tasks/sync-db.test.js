fd9491f1d6a7e83d57c5a264a79867ef18be4dc8
Pedro Biel <structural.eng.biel@gmail.com>
2024-07-09 18:09:15 +0200

    Summary

---
+++ tests/tasks/sync-db.test.js
@@ -0,0 +1,12 @@
+const { syncDB } = require('../../tasks/sync-db')
+
+describe('Pruebas en Sync-DB', () => {
+
+    test('Debe ejecutar el proceso 2 veces', () => {
+
+        syncDB();
+        const times = syncDB();
+//        console.log('Se llamó ', times);
+        expect(times).toBe(2);
+    });
+});
